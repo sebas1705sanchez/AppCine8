eaca37a40626071f49839594472ad706b37fd0d6
IsaC1B <i.cardona1@utp.edu.co>
2024-06-27 16:46:24 -0500

    Ajustes de estilo

--- src/components/TrendingMoviesPreview .jsx
+++ src/components/TrendingMoviesPreview .jsx
@@ -4,9 +4,8 @@ import '../style/movie.css'; // Asegúrate de importar el archivo CSS
 
 const API_KEY = 'c84b15de02b182bd760ca972c743c53f'; // Recuerda reemplazar 'tu_api_key' con tu clave de API de TMDb
 
-const TrendingMoviesPreview = (props) => {
+const TrendingMoviesPreview = () => {
   const [movies, setMovies] = useState([]);
-  const [ststartIndexart, setStartIndex] = useState(0) 
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -24,19 +23,6 @@ const TrendingMoviesPreview = (props) => {
     fetchMovies();
   }, [])
 
-  
- 
-
-  const siguienteTestimonio = () => {
-    setStartIndex((prevIndex) => Math.min(prevIndex + 1, movies.length - 5)); // Asegurarse de no exceder el límite
-    console.log("siguiente")
-  };
-
-  const anteriorTestimonio = () => {
-    //setIndixInicial((prevIndice) => (prevIndice - 1 + 20) % 20);
-    console.log("atras")
-  };
-  
   return (
     <div id="trendingPreview">
       <h2 >Trending Movies Today</h2>
@@ -52,9 +38,6 @@ const TrendingMoviesPreview = (props) => {
             </div>
           ))}
         </div>
-        
-          <button onClick={anteriorTestimonio}>Anterior</button>
-          <button onClick={siguienteTestimonio}>Siguiente</button>
         
       </div>
     </div>
