32c6ddafbe7524f7b9b5848e1afec58e7c8c4412
Jeffer512 <140467995+Jeffer512@users.noreply.github.com>
2024-06-28 15:44:36 -0500

    Integrate files

---
+++ src/components/ResultsPage.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import '../css/ResultsPage.css';
+
+function ResultsPage({page, type, search, index}) {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        // Fetch movies based on the user's search query
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    `https://api.themoviedb.org/3/search/${type}?api_key=fbd275a080fd3aac51146bb6a6946f33&query=${search}&page=${page}`
+                );
+                const data2 = await response.json();
+                setData(data2.results);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+
+    }, [page, type, search, index]);
+
+    const newData = data.slice(index, index + 10);
+    console.log(newData);
+    return (
+        <div className="Container">
+            <div className="Movie">
+                {newData.map((element) => (
+                    <div key={element.id} className="">
+                        <img className="movie-images"
+                            src={`https://image.tmdb.org/t/p/w500${element.poster_path}` + `https://image.tmdb.org/t/p/w500${element.profile_path}` }
+                            alt={element.title}
+                        />
+                          <div className="info-container">
+                              <h3>{element.title}</h3>
+                              <p>{element.release_date}</p>
+                          </div>
+                        <p className="title">{element.title}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+
+
+export default ResultsPage;
